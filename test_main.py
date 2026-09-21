87ab535494f8c10309d970b152e845783ba2cc2e
ShiqianZhang041031 <shiqianzhang666@gmail.com>
2026-09-21 16:28:35 -0500

    Initial commit

---
+++ test_main.py
@@ -0,0 +1,17 @@
+from main import *
+
+# Feel free to expand and add your own tests here.
+# Doing so won't impact the gradescope autograder tests (gradescope uses
+# its own copy of this file so any changes you make here won't affect it).
+
+# 5 pts
+def test_quadratic_multiply():
+    assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert quadratic_multiply(BinaryNumber(3), BinaryNumber(4)) == 3*4
+    assert quadratic_multiply(BinaryNumber(9), BinaryNumber(13)) == 9*13
+
+# 5 pts
+def test_subquadratic_multiply():
+    assert subquadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert subquadratic_multiply(BinaryNumber(3), BinaryNumber(4)) == 3*4
+    assert subquadratic_multiply(BinaryNumber(9), BinaryNumber(13)) == 9*13
